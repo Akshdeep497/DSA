aa582c11a64e466cd87f7ecf522f260cd0809c77
Akshdeep497 <136171308+Akshdeep497@users.noreply.github.com>
2023-08-30 23:52:13 +0530

    Create 29_DFS(depth_first_search).c

---
+++ 29_DFS(depth_first_search).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int visited[7] = {0, 0, 0, 0, 0, 0, 0};
+int a[7][7] = {
+    {0, 1, 1, 1, 0, 0, 0},
+    {1, 0, 1, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 0},
+    {1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 1, 1, 0, 1, 1},
+    {0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0}};
+void dfs(int i)
+{
+    visited[i] = 1;
+    printf(" %d", i);
+    for (int j = 0; j < 7; j++)
+    {
+        if (a[i][j] == 1 && visited[j] == 0)
+        {
+
+            visited[j] = 1;
+            dfs(j);
+        }
+    }
+}
+int main()
+{
+    dfs(6);
+
+    return 0;
+}
